e48fd57691912b9501bf5ef0e2f874c9368f2fab
wszhang <wszhang@Ctrip.com>
2017-09-11 17:25:41 +0800

    add get and post

--- src/models/model.js
+++ src/models/model.js
@@ -12,6 +12,50 @@ var model = {
                 callback && typeof callback == "function" && callback(ret);
             }
         });
+    },
+    
+    //get 请求
+    get:function(url,params,callback){
+        $.ajax({
+            url:url,
+            type:"GET",
+            data:params,
+            dataType:"json",
+            success:function(ret){
+                //ret是接口返回的数据，传到controller中
+                callback && typeof callback == "function" && callback({
+                    isSuccess:true,
+                    ret:ret
+                });
+            },
+            error:function(err){                
+                callback && typeof callback == "function" && callback({
+                    isSuccess:false
+                });
+            }
+        });
+    },
+    
+    //post 请求
+    post:function(url,params,callback){
+        $.ajax({
+            url:url,
+            type:"POST",
+            data:params,
+            dataType:"json",
+            success:function(ret){
+                //ret是接口返回的数据，传到controller中                
+                callback && typeof callback == "function" && callback({
+                    isSuccess:true,
+                    ret:ret
+                });
+            },
+            error:function(err){                
+                callback && typeof callback == "function" && callback({
+                    isSuccess:false
+                });
+            }
+        });        
     }
 };
 
